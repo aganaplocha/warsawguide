0e612e0cd2dc4428c78c73470759ef2f5dd39239
aganaplocha <aganaplocha@gmail.com>
2016-03-13 22:42:31 +0100

    Added Firebase connection

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,5 +1,10 @@
 angular.module('warsawApp', [])
     .controller('MainController', function() {
 
-
     });
+
+	var app = angular.module("warsawApp", ["firebase"]);
+
+	app.controller('MainController', function($scope, $firebaseObject) {
+	    var ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
+	});
