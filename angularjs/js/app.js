d5d4090020eb43d8f028e73a77ebf39a23451779
Agnieszka Naplocha <naplocha@adobe.com>
2016-03-16 15:05:05 +0100

    Two way binding count variable

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,15 +1,7 @@
-angular.module('warsawApp', [])
-    .controller('MainController', function() {
-    	 var counterRef = ref.child('attractions/malinova/counter');
-    	 $firebaseObject(counterRef).$bindTo($scope, 'mainCtrl.count');
+var app = angular.module("warsawApp", ["firebase"]);
 
-    });
-
-
-	var app = angular.module("warsawApp", ["firebase"]);
-	
-	app.controller('MainController', function($scope, $firebaseObject) {
-	    	var ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
-
-	});
-
+app.controller('MainController', function($scope, $firebaseObject) {
+    var ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
+    var counterRef = ref.child('attractions/malinova/counter');
+    $firebaseObject(counterRef).$bindTo($scope, 'mainCtrl.count');
+});
--- angularjs/js/counter.js
+++ angularjs/js/counter.js
@@ -2,21 +2,16 @@ angular.module('warsawApp')
     .directive('warsawCounter', function() {
         return {
             scope: {
-                this.count: "=mainCtrl.count"
+                count: "=", //The = means two-way bind. Whenever the variable is changed outside, the change will be copied inside and the other way around.
             },
-            
+            controller: 'mainCtrl', 
             restrict: 'E',
             controllerAs: 'ctrl',
             templateUrl: '../counter.html',
-            bindToController: true,
-            bindToController: {
-                address: '='
-              },
-             
+            bindToController: true, //This means: Not only put the count on the $scope of the directive, but also on the controller.
+            
 
         };
     });
-
-
 
 
