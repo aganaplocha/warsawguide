42f07d31bc3288a2c46d65689d90b0e46f4c5633
Aga Naplocha <aganaplocha@gmail.com>
2016-03-17 09:46:39 +0100

    Small changes in counter.js

--- angularjs/js/counter.js
+++ angularjs/js/counter.js
@@ -1,17 +1,15 @@
 angular.module('warsawApp')
     .directive('warsawCounter', function() {
         return {
+            restrict: 'E',
             scope: {
                 count: "=", //The = means two-way bind. Whenever the variable is changed outside, the change will be copied inside and the other way around.
             },
-            controller: 'mainCtrl', 
-            restrict: 'E',
+            controller: function() {
+
+            },
             controllerAs: 'ctrl',
             templateUrl: '../counter.html',
             bindToController: true, //This means: Not only put the count on the $scope of the directive, but also on the controller.
-            
-
         };
     });
-
-
