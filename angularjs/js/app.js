bfead5c339f7a91be462500f6c2835fb8cc20475
Aga Naplocha <aganaplocha@gmail.com>
2016-03-18 18:21:26 +0100

    Fixes for Firebase database

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,7 +1,10 @@
 var app = angular.module("warsawApp", ["firebase"]);
-
+ "use strict";
+ 
 app.controller('MainController', function($scope, $firebaseObject) {
-    var ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
-    var counterRef = ref.child('attractions/malinova/counter');
-    $firebaseObject(counterRef).$bindTo($scope, 'mainCtrl.count');
+    let ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
+	let malinovaRef = ref.child('attractions/malinova');
+	$firebaseObject(malinovaRef).$bindTo($scope, 'mainCtrl.data');
+
 });
+
