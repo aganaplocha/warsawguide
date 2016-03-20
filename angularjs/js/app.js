eda4cce19611d9356c6992d3d653056155efa8e2
Aga Naplocha <aganaplocha@gmail.com>
2016-03-20 19:06:58 +0100

    Added routing for main, malinova and test

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,14 +1,31 @@
 "use strict";
-var app = angular.module("warsawApp", ["firebase"]);
+var app = angular.module('warsawApp', ['ngRoute','firebase']);
  
+ app.controller('MainController', function() {
+
+});
+
 app.controller('MalinovaController', function($scope, $firebaseObject) {
     let ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
 	let malinovaRef = ref.child('attractions/malinova');
-	$firebaseObject(malinovaRef).$bindTo($scope, 'mainCtrl.data');
+	$firebaseObject(malinovaRef).$bindTo($scope, 'malinovaCtrl.data');
 
 });
 
-app.controller('MainController', function() {
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
 
-});
+    when('/', {
+        templateUrl: '../main.html',
+        controller: 'MainController',
+        controllerAs: 'mainCtrl'
+      }).
+    when('/malinova', {
+        templateUrl: '../text.html',
+        controllerAs: 'malinovaCtrl'
+    }).
+    when('/test', {
+        templateUrl: '../test.html'
+    });
+ }]);
  
