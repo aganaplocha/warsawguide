0305a05a0c6e3fea7f456300116c2c8c74d98c34
Aga Naplocha <aganaplocha@gmail.com>
2016-03-24 14:17:43 +0100

    Login panel created

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,16 +1,29 @@
 "use strict";
-var app = angular.module('warsawApp', ['ngRoute','firebase']);
- 
-app.controller('MainController', function() {
+var app = angular.module('warsawApp', ['ngRoute', 'firebase']);
+
+app.controller('MainController', function($scope) {
+   
+};
 
 });
 
 app.controller('MalinovaController', function($scope, $firebaseObject) {
     let ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
-	let malinovaRef = ref.child('attractions/malinova');
-	$firebaseObject(malinovaRef).$bindTo($scope, 'malinovaCtrl.data');
+    let malinovaRef = ref.child('attractions/malinova');
+    $firebaseObject(malinovaRef).$bindTo($scope, 'malinovaCtrl.data');
 
+    // ref.createUser({
+    //     email: "bobtony@firebase.com",
+    //     password: "correcthorsebatterystaple"
+    // }, function(error, userData) {
+    //     if (error) {
+    //         console.log("Error creating user:", error);
+    //     } else {
+    //         console.log("Successfully created user account with uid:", userData.uid);
+    //     }
+    // });
 });
+
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
@@ -19,12 +32,11 @@ app.config(['$routeProvider', function($routeProvider) {
         templateUrl: '../main.html',
         controller: 'MainController',
         controllerAs: 'mainCtrl'
-      }).
+    }).
     when('/malinova', {
         templateUrl: '../malinova.html',
         controller: 'MalinovaController',
         controllerAs: 'malinovaCtrl'
     });
-   
- }]);
- 
+
+}]);
---
+++ angularjs/js/loginpanel.js
@@ -0,0 +1,15 @@
+angular.module('warsawApp')
+    .directive('loginPanel', function() {
+        return {
+            restrict: 'E',
+            scope: {
+
+            },
+            controller: function() {
+               
+
+            },
+            controllerAs: 'ctrl',
+            templateUrl: '../loginpanel.html',
+        };
+    });
