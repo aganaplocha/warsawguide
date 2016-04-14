e5f913fc6d45cdb1bb48d4bba7507a79102b91ec
Aga Naplocha <aganaplocha@gmail.com>
2016-04-14 21:59:05 +0200

    User authentication basic code added

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -1,12 +1,35 @@
 "use strict";
-var app = angular.module('warsawApp', ['ngRoute','ngAnimate','firebase']);
+var app = angular.module('warsawApp', ['ngRoute', 'ngAnimate', 'firebase']);
 
 app.controller('MainController', function($scope) {
+    var ref = new Firebase("https://blinding-inferno-6187.firebaseio.com");
+    ref.createUser({
+        email: "bobtony@firebase.com",
+        password: "correcthorsebatterystaple"
+    }, function(error, userData) {
+        if (error) {
+            console.log("Error creating user:", error);
+        } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+        }
+    });
     this.textWhole = false; // initially not displayed
     this.toggleReadMore = function() {
         this.textWhole = !this.textWhole;
     };
+
+    ref.authWithPassword({
+        email: "bobtony@firebase.com",
+        password: "correcthorsebatterystaple"
+    }, function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+        }
+    });
 });
+
 
 app.controller('MalinovaController', function($scope, $firebaseObject) {
     let ref = new Firebase('https://blinding-inferno-6187.firebaseio.com');
