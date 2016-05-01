2f7b76c3fe323460d338648f645e936dae86ebb9
Aga Naplocha <aganaplocha@gmail.com>
2016-05-01 14:23:38 +0200

    Password error (using promises)

--- angularjs/js/app.js
+++ angularjs/js/app.js
@@ -4,41 +4,40 @@ app.constant('warsawFirebaseUrl','https://blinding-inferno-6187.firebaseio.com')
 app.factory('warsawFirebaseRef', function (warsawFirebaseUrl){
     return new Firebase(warsawFirebaseUrl);;
 });
-var AccountService = function (warsawFirebaseRef) {
+var AccountService = function (warsawFirebaseRef, $q) {
    this.ref = warsawFirebaseRef;
    this.email = null;
+   this.$q = $q;
 };
 
 AccountService.prototype.createUser = function() {
 
 };
 AccountService.prototype.login = function(email, password) {
+    var deferred = this.$q.defer();
     this.ref.authWithPassword({
         email: email,
         password: password
     }, (error, authData) => {  // arrow function
         if (error) {
-            console.log("Login Failed!", error);
+            deferred.reject(error);
         } else {
             this.email = authData.password.email;
+            deferred.resolve(authData);
             console.log("Authenticated successfully with payload:", authData);
-            console.log(authWithPassword.uid);
         }
-    });        
+    });      
+    return deferred.promise;  
 };  
 app.service('warsawAccountService', AccountService);
 
 
 app.controller('MainController', function($scope, warsawAccountService) {
-    
     this.accountService = warsawAccountService; // adding service for user account
-
     this.textWhole = false; // initially not displayed
     this.toggleReadMore = function() {
         this.textWhole = !this.textWhole;
     };
-
-
 });
 
 
--- angularjs/js/navbar.js
+++ angularjs/js/navbar.js
@@ -2,13 +2,21 @@ var NavbarController = function(warsawAccountService) {
     this.accountService = warsawAccountService;
     this.email = '';
     this.password = '';
+    this.passwordError = null;
     this.loginDisplayed = false; // initially not displayed
 };
 NavbarController.prototype.toggleLoginPanel = function() { // renamed it toggle, because depending on the current state, it may also hide, not just show
     this.loginDisplayed = !this.loginDisplayed; // this is a shorter way to express you if..else - it just flips the value of the variable
 };
 NavbarController.prototype.login = function() { 
-    this.accountService.login(this.email, this.password);
+    var promise = this.accountService.login(this.email, this.password);
+    promise.then((authData) => {
+        this.passwordError = null;
+        this.loginDisplayed = false;
+    }, 
+    (error) => {
+        this.passwordError = error.message;
+    });
 };
 angular.module('warsawApp')
     .directive('warsawNavbar', function() {
